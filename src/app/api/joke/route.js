32a941d93ae1ce98c3ce7dc7375a8616e7f145aa
MarioM <a20300683@live.ceti.mx>
2024-10-14 17:52:16 -0600

    Implementation of API GPT-4o mini

---
+++ src/app/api/joke/route.js
@@ -0,0 +1,45 @@
+import { NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+export async function POST(request) {
+  try {
+    const { topic } = await request.json();
+
+    if (!topic ) {
+      return NextResponse.json({ error: 'Faltan los parámetros "topic" ' }, { status: 400 });
+    }
+
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    if (!openai.apiKey) {
+      return NextResponse.json({ error: 'Falta la clave API de OpenAI' }, { status: 500 });
+    }
+
+    const messages = [
+      {
+        role: 'system',
+        content: `Eres un asistente de IA especializado en contar chistes. Solo devuelves el chiste sin información adicional.`,
+      },
+      {
+        role: 'user',
+        content: `Por favor, cuéntame un chiste sobre las siguientes categorías: ${topic}`,
+      },
+    ];
+
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: messages,
+      max_tokens: 150,
+      temperature: 1.0, // Ajusta la temperatura para más variedad
+    });
+
+    const joke = response.choices[0].message.content.trim();
+
+    return NextResponse.json({ joke });
+  } catch (error) {
+    console.error('Error al generar el chiste:', error);
+    return NextResponse.json({ error: 'Error al generar el chiste' }, { status: 500 });
+  }
+}
--- src/app/generate/page.js
+++ src/app/generate/page.js
@@ -2,12 +2,15 @@ import Footer from "@/components/Footer"
 import Header from "@/components/Header"
 import JokeGenerator from "@/components/JokeGenerator"
 
+
 export default function ChistePage() {
   return (
    <>
    <Header></Header>
 
    <JokeGenerator></JokeGenerator>
+
+ 
 
    <Footer></Footer>
    </>
